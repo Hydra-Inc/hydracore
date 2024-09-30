8b8c15ff44efba08f9e9140b8a22ed0e3d125ca4
Hydra-Inc <hydra-core@yandex.com>
2024-10-01 00:23:51 +0300

    JO v2

--- lib/src/hydracore/heroes3/templates/jebus_outcast/v2_96.py
+++ lib/src/hydracore/heroes3/templates/jebus_outcast/v2_96.py
@@ -5,6 +5,11 @@ class JebusOutcast_v_2_96(JebusOutcast):
     def version(self) -> str:
         return '2.96 test v1'
 
+class JebusOutcast_v_2_96_v2(JebusOutcast):
+
+    def version(self) -> str:
+        return '2.96 test v2'
+
 class JebusOutcast_v_2_96a(JebusOutcast):
 
     def version(self) -> str:
